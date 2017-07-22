04f2adeaac372b039d9db1831e718dc20ee4ac3e
Blasapo <blasapo@gmail.com>
2017-07-22 20:56:40 +0200

    basic list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DataList from './list.js';
 
+const ListProducts = [
+  {
+    name:'Calabacines',
+    quantity:1,
+    category:'Verdura',
+    description:"loremipsun",
+    price:3
+  },
+
+  {
+    name:"Brocoli",
+    quantity:1,
+    category:"Verdura",
+    description:"loremipsun",
+    price:3
+  },
+  {
+    name:"leche",
+    quantity:6,
+    category:"Desayuno",
+    price:1.5
+  },
+]
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+ render() {
+   return (
+     <div>
+
+     </div>
+   );
+ }
 }
 
 export default App;
